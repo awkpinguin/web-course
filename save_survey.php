3f92b3ced589d07cf269b6d2e69ffd4a6e6b6c0f
awkpinguin <nastiakaplunova@yandex.ru>
2022-03-19 16:55:37 +0300

    сделала задание 2 из 3 лабы по вебу

--- save_survey.php
+++ save_survey.php
@@ -5,7 +5,7 @@ function getRequestParameter(string $key): ?string
     return $_GET[$key] ?? null;
 }
 
-$identifier = getRequestParameter('identifier');
+$identifier=getRequestParameter('identifier');
 
 if ($identifier === null || $identifier === "")
 {
@@ -13,12 +13,18 @@ if ($identifier === null || $identifier === "")
 }
 else
 {
-    $arr=str_split('identifier');
+    $arr = str_split('identifier');
     if (!ctype_alpha($arr[0]))
         echo "NO, идентификатор должен начинаться с буквы";
-    for($i = 0; $i < count($arr); ++$i)
+    else
     {
-        if (ctype_alpha($arr[$i]) || (is_numeric($arr[$i])))
-            echo "YES, идентификатор верный";
+        for ($i = 1; $i < count($arr); ++$i)
+        {
+            if (ctype_alpha($arr[$i]) || (is_numeric($arr[$i])))
+                $answer = "YES, идентификатор верный";
+            else
+                $answer = "NO, идентификатор неверный";
+        }
+        echo $answer;
     }
 }
