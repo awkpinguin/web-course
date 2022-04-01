c10ff424b2844028594818ec32425bacfba24a50
awkpinguin <nastiakaplunova@yandex.ru>
2022-04-01 09:53:44 +0300

    Изменила способ перевода курсора на новую строку

--- UserSurvey/src/SurveyPrinter.php
+++ UserSurvey/src/SurveyPrinter.php
@@ -3,10 +3,10 @@ class SurveyPrinter
 {
     function printArguments($arguments)
     {
-        $infoStr = "First name: " . $arguments[0] . PHP_EOL;
-        $infoStr = $infoStr . 'Last name: ' . $arguments[1] . PHP_EOL;
-        $infoStr = $infoStr . 'Email: ' . $arguments[2] . PHP_EOL;
-        $infoStr = $infoStr . 'Age: ' . $arguments[3];
+        $infoStr = "First name: " . $arguments[0] . "<br/>";
+        $infoStr = $infoStr . 'Last name: ' . $arguments[1] . "<br/>";
+        $infoStr = $infoStr . 'Email: ' . $arguments[2] . "<br/>";
+        $infoStr = $infoStr . 'Age: ' . $arguments[3] . "<br/>";
         echo $infoStr;
     }
 }
